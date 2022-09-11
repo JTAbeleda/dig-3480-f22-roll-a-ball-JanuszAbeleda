ac80fd4ba50d87d8c5d6238f3ca850b5a69b4629
JTAbeleda <jt.abeleda@knights.ucf.edu>
2022-09-11 18:22:19 -0400

    Text Added to game and name change again.
    Finished the game and renamed it again.

--- dig-3480_Ball_Tutorial-Janusz-Abeleda/Assets/Scripts/PlayerController.cs
+++ dig-3480_Ball_Tutorial-Janusz-Abeleda/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
 
     private float movementX;
     private float movementY;
@@ -16,6 +20,10 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void OnMove(InputValue movementValue)
@@ -24,6 +32,15 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+    
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >=12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     private void FixedUpdate()
@@ -38,6 +55,9 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp")) 
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
         }
     }
 }
